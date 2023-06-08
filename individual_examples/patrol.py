1cfee962c9c4f3e2388cd18fa3588337cb4a0df5
KhounborineII <khounborineii@hendrix.edu>
2023-06-08 12:08:26 -0500

    thread test

--- individual_examples/patrol.py
+++ individual_examples/patrol.py
@@ -1,4 +1,5 @@
 import rclpy
+import threading
 from rclpy.action import ActionClient
 from rclpy.node import Node
 
@@ -23,7 +24,7 @@ class DriveDistanceActionClient(Node):
         self._action_client = ActionClient(
             self, DriveDistance, 'drive_distance')
 
-    def send_goal(self, distance=0.5, max_translation_speed=0.15):
+    def send_goal(self, distance=1.0, max_translation_speed=0.5):
         '''
         Purpose
         -------
@@ -33,9 +34,9 @@ class DriveDistanceActionClient(Node):
         goal_msg = DriveDistance.Goal()
         goal_msg.distance = distance
         goal_msg.max_translation_speed = max_translation_speed
-
+        print("Wait 1")
         self._action_client.wait_for_server()
-
+        print("Send Message (1)")
         self._send_goal_future = self._action_client.send_goal_async(goal_msg)
 
         self._send_goal_future.add_done_callback(self.goal_response_callback)
@@ -84,7 +85,7 @@ class RotateActionClient(Node):
         super().__init__('rotate_action_client')
         self._action_client = ActionClient(self, RotateAngle, 'rotate_angle')
 
-    def send_goal(self, angle=1.0, max_rotation_speed=0.5):
+    def send_goal(self, angle=3.3, max_rotation_speed=0.5):
         '''
         Purpose
         -------
@@ -94,8 +95,9 @@ class RotateActionClient(Node):
         goal_msg = RotateAngle.Goal()
         goal_msg.angle = angle 
         goal_msg.max_rotation_speed = max_rotation_speed
-
+        print("Wait 2")
         self._action_client.wait_for_server()
+        print("Send Message (2)")
         self._send_goal_future = self._action_client.send_goal_async(goal_msg)
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
@@ -126,26 +128,56 @@ class RotateActionClient(Node):
         self.get_logger().info('Result: {0}'.format(result))
         #rclpy.shutdown()
 
-def main(args=None):
-    rclpy.init(args=args)
 
-    action_client = DriveDistanceActionClient()
-    action_client2 = RotateActionClient()
+def drive_thread(finished, ros_ready):
+    print("starting")
+    rclpy.init(args=None)
+    print("init done")
 
-    dist  = 1.0
-    speed = 0.25
+    driving = DriveDistanceActionClient()
+    print("node set up; awaiting ROS2 startup...")
+    executor = rclpy.get_global_executor()
+    executor.add_node(driving)
+    while executor.context.ok() and not finished.is_set():
+        executor.spin_once()
+        if driving.ros_issuing_callbacks():
+            ros_ready.set()
+    driving.reset()
+    #rclpy.shutdown()
 
-    action_client.send_goal(dist, speed)
-    rclpy.spin(action_client)
-    try:
-        rclpy.spin(action_client)
-        rclpy.spin(action_client2)
-    except KeyboardInterrupt:
-        print('\nCaught keyboard interrupt')
-    finally:
-        print("Done")
-        rclpy.shutdown()
+def spin_thread(finished, ros_ready):
+    print("starting")
+    rclpy.init(args=None)
+    print("init done")
+
+    turning = RotateActionClient()
+    print("node set up; awaiting ROS2 startup...")
+    executor = rclpy.get_global_executor()
+    executor.add_node(turning)
+    while executor.context.ok() and not finished.is_set():
+        executor.spin_once()
+        if turning.ros_issuing_callbacks():
+            ros_ready.set()
+    turning.reset()
+    rclpy.shutdown()
+
+
+def input_thread(finished, ros_ready):
+    ros_ready.wait()
+    user = input("Type anything to exit")
+    finished.set()
 
 
 if __name__ == '__main__':
-    main()
+    finished = threading.Event()
+    ros_ready = threading.Event()
+
+    dt = threading.Thread(target=drive_thread, args=(finished,ros_ready))
+    st = threading.Thread(target=spin_thread, args=(finished,ros_ready))
+    it = threading.Thread(target=input_thread, args=(finished,ros_ready))
+    dt.start()
+    it.start()
+    st.start()
+    dt.join()
+    it.join()
+    st.join()
